9513bad6db6d563144df36fe5cd0dca91ed5a5e4
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2024-02-15 09:35:57 +0530

    Delete Major/In.sql

--- Major/In.sql
+++
@@ -1,7 +0,0 @@
-select * from table_name
-where Column_name In('Aus','In');
---So here we have a specific element value focus --
-
-select * from table_name
-where Column_name not In('Aus','In');
---So here excluded--
