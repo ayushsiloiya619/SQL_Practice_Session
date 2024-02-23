2b61c1a0e678998c052ad784675a87e331d451be
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2024-02-23 09:19:59 +0530

    trim_sql

---
+++ Major/ltrim_rtrim.sql
@@ -0,0 +1,10 @@
+--RTRIM/LTRIM/LEFT/RIGHT/LOWER/UPPER--
+
+select LEFT('Ayush Siloiya',5);
+SELECT RIGHT('Ayush Siloiya',7);
+select UPPER('ayush siloiya');
+select LOWER('AYUSH SILOIYA');
+select LTRIM('     Ayush Siloiya');
+select RTRIM('Ayush Siloiya       ');
+select SUBSTRING('Ayush Siloiya',1,5);
+select REPLACE('Ayush','Ayush','Siloiya');
